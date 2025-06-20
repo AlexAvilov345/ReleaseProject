e3467ea1bbdaa09354b2cc5e9571bbb14c11def0
AlexAvilov345 <avilovoleksij@gmail.com>
2025-06-20 18:04:25 +0300

    new-commit

--- script.js
+++ script.js
@@ -20,19 +20,6 @@ window.addEventListener("click", (event) => {
 });
 
 
-let lastScroll = 0
-  let header = document.querySelector('.header')
 
-  window.addEventListener('scroll', () => {
-    let currentScroll = window.scrollY
-
-    if (currentScroll > lastScroll && currentScroll > 50) {
-      header.classList.add('hidden')
-    } else {
-      header.classList.remove('hidden')
-    }
-
-    lastScroll = currentScroll
-  })
   
   
