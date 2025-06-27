8e8c197e2b76cbef54c9894167fb7105a77e612d
AlexAvilov345 <avilovoleksij@gmail.com>
2025-06-27 19:31:51 +0300

    new-commit

--- cart.js
+++ cart.js
@@ -1,24 +1,79 @@
 let container_card = document.querySelector(".container-card");
 
+let counter = 0;
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
+cart_counter = localStorage.getItem("counter") || 0;
+cart_counterHTML = document.querySelector(".cart-counter")
 
 if (cart.length === 0) {
   container_card.innerHTML = `<p class="empty-cart-message">Кошик порожній</p>`;
 } else {
   cart.forEach(card => {
     let cardHTML = `
+    <div class="cart-container">
       <div class="cart-card">
+      <div class="card mb-3">
         <img src="${card.img}" alt="${card.title}" class="cart-card-img" />
         <div class="cart-card-info">
           <h2 class="cart-card-title">${card.title}</h2>
           <p class="cart-card-desc">${card.desc}</p>
           <p class="cart-card-price">${card.price}</p>
+           <p class="card-text"><small class="text-muted" data-price = "${card.price}">${card.price}</small></p>
+                 <div class="input-group">
+        <input type="number" class="form-control" placeholder="Amount" min="1"value="1">
+          <button class="btn-btn-outline-secondary" type="button" data-action="minus">-</button>
+          <button class="btn-btn-outline-secondary" type="button" data-action="plus">+</button>    
+          </div>
+          </div>
         </div>
+      </div>
       </div>
     `;
     container_card.insertAdjacentHTML("beforeend", cardHTML);
   });
 }
+document.querySelectorAll("[data-action='minus']").forEach(btn => {
+  btn.addEventListener("click", ()=>{
+   const input = btn.closest(".input-group").querySelector(".form-control");
+    let currentValue = parseInt(input.value);
+    
+
+    if(currentValue > 1){
+      let newValue = currentValue - 1
+      input.value = newValue;
+          const priceElement = btn.closest(".card").querySelector(".text-muted");
+    const unitPrice = parseFloat(priceElement.dataset.price);
+    priceElement.innerHTML -= unitPrice
+
+
+      cart_counter--
+    localStorage.setItem("counter", cart_counter)
+    cart_counterHTML.innerHTML = cart_counter
+    
+    }
+
+  })
+})
+
+document.querySelectorAll("[data-action='plus']").forEach(btn => {
+  btn.addEventListener("click", ()=>{
+   const input = btn.closest(".input-group").querySelector(".form-control");
+    let newValue = parseInt(input.value) + 1;
+    localStorage.setItem("cart-counter", newValue)
+    input.value = localStorage.getItem("cart-counter");
+
+    const priceElement = btn.closest(".card").querySelector(".text-muted");
+    const unitPrice = parseFloat(priceElement.dataset.price);
+    priceElement.innerHTML = unitPrice * newValue
+
+    cart_counter++
+    localStorage.setItem("counter", cart_counter)
+    cart_counterHTML.innerHTML = cart_counter
+
+
+
+  })
+})
 document.querySelector(".clear-cart-btn").addEventListener("click", () => {
   localStorage.removeItem("cart");
   location.reload(); 
--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+let cart_counterHTML = document.querySelector(".cart-counter")
+let cart_counter = localStorage.getItem("counter") || 0;
+cart_counterHTML.innerHTML = cart_counter
+
 function goToCart() {
   window.location.href = "cart.html";
 }
 window.addEventListener("click", (event) => {
   if (event.target.closest("[data-cart]")) {
+    cart_counter++
+      cart_counterHTML.innerHTML = cart_counter
+      localStorage.setItem("counter", cart_counter)
     let btn_bye = event.target.closest("[data-cart]");
     let card = btn_bye.closest(".product-card");
 
@@ -10,7 +17,8 @@ window.addEventListener("click", (event) => {
       title: card.querySelector(".product-name").innerText,
       img: card.querySelector(".product-img").getAttribute("src"),
       desc: card.querySelector(".product-desc").innerText,
-      price: card.querySelector(".product-price").innerText
+      price: card.querySelector(".product-price").innerText,
+      id: card.querySelector(".id").innerText
     };
 
     let cart = JSON.parse(localStorage.getItem("cart")) || [];
@@ -18,8 +26,7 @@ window.addEventListener("click", (event) => {
     localStorage.setItem("cart", JSON.stringify(cart));
   }
 });
+function goToCat() {
+  window.location.href = "catalog.html";
+}
 
-
-
-  
-  
